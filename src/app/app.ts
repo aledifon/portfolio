c0dd5af99ee2093f826f18094d56d8582f114378
aledifon <aledifon89@gmail.com>
2026-04-19 19:25:53 +0200

    Created About & Home Pages Structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+// import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
+// import { ContactComponent } from './pages/contact/contact.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomeComponent, AboutComponent],
   templateUrl: './app.html',
   styleUrl: './app.component.scss'
 })
