6f3477aefceecb79096ffd1d0209616ac2a21f78
aledifon <aledifon89@gmail.com>
2026-04-20 21:25:01 +0200

    Finished initial HTML structure of Home, About & Contact Pages + Created template structure of Projects Page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
-// import { ProjectsComponent } from './pages/projects/projects.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 import { AboutComponent } from './pages/about/about.component';
-// import { ContactComponent } from './pages/contact/contact.component';
+import { ContactComponent } from './pages/contact/contact.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, AboutComponent],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.html',
   styleUrl: './app.component.scss'
 })
