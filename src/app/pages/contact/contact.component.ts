7cb405050c40631a9b4442827e309c39431406d6
aledifon <aledifon89@gmail.com>
2026-05-04 22:40:28 +0200

    Starting with reactive forms on the contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+
+  form = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50)
+    ]),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.email
+    ]),
+    subject: new FormControl(""),
+    message: new FormControl("", [
+      Validators.required      
+    ])
+  });
+
+  onSubmit(){
+    if(this.form.valid){
+      console.info("Form sent: ", this.form.value);
+      console.log(this.form.value.message);
+    }
+    else{
+      console.warn("Invalid form!!");
+    }
+  }
+
+}
