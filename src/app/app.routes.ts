db53c7f076e928e42530519848f492ebca54549e
aledifon <aledifon89@gmail.com>
2026-05-08 14:37:47 +0200

    Added 404 not found page to the routing and finished 404 not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "home", component: HomeComponent},
     {path: "projects", component: ProjectsComponent},
     {path: "about", component: AboutComponent},
-    {path: "contact", component: ContactComponent}    
-    // {path: "**", component: Page404Component}           // Default route in case of typing 
-                                                        // an invalid route
+    {path: "contact", component: ContactComponent},
+    
+    // 404 fallback
+    {path: "**", component: NotFoundComponent}  // Default route in case of typing 
+                                                // an invalid route 
 ];
